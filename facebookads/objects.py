a0f7e7e0d6e3e9cdbc4a722d0554480f3508bfe8
tomwallroth <tomwallroth@gmail.com>
2015-07-30 14:09:29 +0200

    implemented RSVP fetching from events

--- facebookads/objects.py
+++ facebookads/objects.py
@@ -2633,6 +2633,91 @@ class Interest(CannotCreate, CannotDelete, AbstractCrudObject):
         name = 'name'
 
 
+class FacebookUser(AbstractCrudObject, CannotDelete, CannotCreate, CannotUpdate):
+    class Field(object):
+        id = 'id'
+        about = 'about'
+        address = 'address'
+        age_range = 'age_range'
+        bio = 'bio'
+        birthday = 'birthday'
+        context = 'context'
+        currency = 'currency'
+        devices = 'devices'
+        education = 'education'
+        email = 'email'
+        favorite_athletes = 'favorite_athletes'
+        favorite_teams = 'favorite_teams'
+        first_name = 'first_name'
+        gender = 'gender'
+        hometown = 'hometown'
+        inspirational_people = 'inspirational_people'
+        install_type = 'install_type'
+        installed = 'installed'
+        interested_in = 'interested_in'
+        is_shared_login = 'is_shared_login'
+        is_verified = 'is_verified'
+        languages = 'languages'
+        last_name = 'last_name'
+        link = 'link'
+        location = 'location'
+        locale = 'locale'
+        meeting_for = 'meeting_for'
+        middle_name = 'middle_name'
+        name = 'name'
+        name_format = 'name_format'
+        payment_pricepoints = 'payment_pricepoints'
+        test_group = 'test_group'
+        political = 'political'
+        relationship_status = 'relationship_status'
+        religion = 'religion'
+        security_settings = 'security_settings'
+        significant_other = 'significant_other'
+        sports = 'sports'
+        quotes = 'quotes'
+        third_party_id = 'third_party_id'
+        timezone = 'timezone'
+        token_for_business = 'token_for_business'
+        updated_time = 'updated_time'
+        shared_login_upgrade_required_by = 'shared_login_upgrade_required_by'
+        verified = 'verified'
+        video_upload_limits = 'video_upload_limits'
+        viewer_can_send_gift = 'viewer_can_send_gift'
+        website = 'website'
+        work = 'work'
+        public_key = 'public_key'
+        cover = 'cover'
+
+
+class RSVP(AbstractCrudObject, CannotDelete, CannotCreate, CannotUpdate):
+    class Field(FacebookUser.Field):
+        pass
+
+
+class RSVPDeclined(RSVP):
+    @classmethod
+    def get_endpoint(cls):
+        return 'declined'
+
+
+class RSVPAttending(RSVP):
+    @classmethod
+    def get_endpoint(cls):
+        return 'attending'
+
+
+class RSVPMaybe(RSVP):
+    @classmethod
+    def get_endpoint(cls):
+        return 'maybe'
+
+
+class RSVPNoReply(RSVP):
+    @classmethod
+    def get_endpoint(cls):
+        return 'noreply'
+
+
 class Event(AbstractCrudObject):
     class Field(object):
         id = 'id'
@@ -2651,6 +2736,18 @@ class Event(AbstractCrudObject):
     @classmethod
     def get_endpoint(cls):
         return 'event'
+
+    def get_rsvp_declined(self, fields=None, params=None):
+        return self.iterate_edge(RSVPDeclined, fields, params)
+
+    def get_rsvp_attending(self, fields=None, params=None):
+        return self.iterate_edge(RSVPAttending, fields, params)
+
+    def get_rsvp_maybe(self, fields=None, params=None):
+        return self.iterate_edge(RSVPMaybe, fields, params)
+
+    def get_rsvp_noreply(self, fields=None, params=None):
+        return self.iterate_edge(RSVPNoReply, fields, params)
 
 
 class PageEvents(Event):
