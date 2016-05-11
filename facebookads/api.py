753f8f7a54be8098ca7b150c9060f3f800b42a99
Lukas Benedix <lbenedix@l3q.de>
2016-05-11 17:04:55 +0200

    added type check before accessing json_body as a dictionary

--- facebookads/api.py
+++ facebookads/api.py
@@ -91,7 +91,7 @@ class FacebookResponse(object):
         json_body = self.json()
 
         # If the data attribute is HTML and contains error in title, this call failed.
-        if 'data' in json_body and type(json_body['data']) == str:
+        if isinstance(json_body, collections.Mapping) and 'data' in json_body and type(json_body['data']) == str:
             if (json_body['data'].startswith('<!DOCTYPE html>')
                     and '<title>Facebook | Error</title>' in json_body['data']):
                 return False
