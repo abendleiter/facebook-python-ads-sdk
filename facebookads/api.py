6bfa5eb4a72de9803d20b134897748840512e4d3
Tom Wallroth <tom@abend.net>
2016-11-22 15:54:16 +0100

    properly detecting FacebookBadResponseErrors

--- facebookads/api.py
+++ facebookads/api.py
@@ -170,6 +170,7 @@ class FacebookResponse(object):
         an appropriate debug message.
         """
         if self.is_failure():
+            FacebookBadResponseError.check_bad_response(self)
             request_error = FacebookRequestError(
                 "Call was not successful",
                 self._call,
