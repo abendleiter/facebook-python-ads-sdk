34e204dbaa701a236de762097b96b68f56d3e916
bollogo <bjoernkutzner@yahoo.com>
2018-05-15 11:41:03 +0200

    Changed api version down to v2.10 for page permissions. This is only temporary and soon we will migrate properly to version v2.11

--- facebookads/adobjects/page.py
+++ facebookads/adobjects/page.py
@@ -126,11 +126,11 @@ class Page(CannotCreate, CannotDelete, CannotUpdate, AbstractPage):
 
 
 class BusinessManagerPage(AbstractPage):
-
     class Field(AbstractPage.CommonField, AbstractPage.AdminOnlyField):
         pass
 
     def cancel_page_permission_request(self):
+        self._api._api_version = 'v2.10'
         request = FacebookRequest(
             node_id=self.get_parent_id_assured(),
             endpoint=self.get_endpoint(),
@@ -141,13 +141,16 @@ class BusinessManagerPage(AbstractPage):
         request.execute()
 
     def get_user_permissions(self, fields=None, params=None):
+        self._api._api_version = 'v2.10'
         return self.iterate_edge(UserPagePermission, fields, params)
 
     def add_user_permission(self, user_id, business_id, role):
+        self._api._api_version = 'v2.10'
         permission = UserPagePermission(parent_id=self.get_id_assured(), api=self.get_api())
         permission.remote_create(params={'user': user_id, 'business': business_id, 'role': role})
 
     def remove_user_permission(self, user_id, business_id):
+        self._api._api_version = 'v2.10'
         request = FacebookRequest(
             node_id=self.get_id_assured(),
             method='DELETE',
