ddf7c5b0178a8ddd0ee51c1d2109504766e05764
bollogo <bjoernkutzner@yahoo.com>
2018-06-13 09:42:43 +0200

    set api version to v2.10 and revert back to v2.11 after the request

--- facebookads/adobjects/page.py
+++ facebookads/adobjects/page.py
@@ -130,6 +130,7 @@ class BusinessManagerPage(AbstractPage):
         pass
 
     def cancel_page_permission_request(self):
+        self._api._api_version = 'v2.10'
         request = FacebookRequest(
             node_id=self.get_parent_id_assured(),
             endpoint=self.get_endpoint(),
@@ -138,15 +139,22 @@ class BusinessManagerPage(AbstractPage):
         )
         request.add_params({'page_id': self.get_id_assured()})
         request.execute()
+        self._api._api_version = 'v2.11'
 
     def get_user_permissions(self, fields=None, params=None):
-        return self.iterate_edge(UserPagePermission, fields, params)
+        self._api._api_version = 'v2.10'
+        cursor = self.iterate_edge(UserPagePermission, fields, params)
+        self._api._api_version = 'v2.11'
+        return cursor
 
     def add_user_permission(self, user_id, business_id, role):
+        self._api._api_version = 'v2.10'
         permission = UserPagePermission(parent_id=self.get_id_assured(), api=self.get_api())
         permission.remote_create(params={'user': user_id, 'business': business_id, 'role': role})
+        self._api._api_version = 'v2.11'
 
     def remove_user_permission(self, user_id, business_id):
+        self._api._api_version = 'v2.10'
         request = FacebookRequest(
             node_id=self.get_id_assured(),
             method='DELETE',
@@ -155,3 +163,4 @@ class BusinessManagerPage(AbstractPage):
         )
         request.add_params({'user': user_id, 'business': business_id})
         request.execute()
+        self._api._api_version = 'v2.11'
